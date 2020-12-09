f40f49bfc3b0ba8caff4df6703938bc33ba2f06a
thurstonzd <thurstonzd@g.cofc.edu>
2020-12-09 14:42:24 -0500

    Update fix_chars.py

--- fix_chars.py
+++ fix_chars.py
@@ -1,10 +1,9 @@
 from simple_recipes.db import get_connection, get_cursor
 
-cn = get_connection()
-cur = get_cursor(cn)
+with get_connection() as cn:
+    with get_cursor(cn) as cur:
+        sql = ( "UPDATE recipe_instructions "
+                "SET instruction = REGEXP_REPLACE(REGEXP_REPLACE(instruction, '(\d)ø', '\1°', 'g'), '(\D)ø(\D)', '\1o\2', 'g') "
+                "WHERE instruction LIKE '%ø%';")
 
-sql = ( "UPDATE recipe_instructions "
-        "SET instruction = REGEXP_REPLACE(REGEXP_REPLACE(instruction, '(\d)ø', '\1°', 'g'), '(\D)ø(\D)', '\1o\2', 'g') "
-        "WHERE instruction LIKE '%ø%';")
-
-cur.execute(sql)
+        cur.execute(sql)
